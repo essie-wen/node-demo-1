b532c8d15eb4779a06682a09ca730e4bc1d009d9
Essie <283135364@qq.com>
2019-10-07 22:18:25 +0800

    V2

--- server.js
+++ server.js
@@ -26,6 +26,14 @@ var server = http.createServer(function(request, response) {
   if (path === "/") {
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/html;charset=utf-8");
+    response.write(`
+    <!DOCTYPE html>
+    <link rel="stylesheet" href="/style.css" >
+      <h1>你好</h1>`);
+    response.end();
+  } else if (path === "/style.css") {
+    response.statusCode = 200;
+    response.setHeader("Content-Type", "text/css;charset=utf-8");
     response.write(`h1{color: red;}`);
     response.end();
   } else {
